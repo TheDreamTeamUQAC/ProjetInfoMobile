517fb51d4e3e6f6a80e9d15a02aa14d8ad14872a
Tommy Duperré <duptom4@gmail.com>
2017-02-28 19:43:11 -0500

    Commit premier clean.

--- RunTimerApp/app/src/main/java/dtuqac/runtimerapp/MainActivity.java
+++ RunTimerApp/app/src/main/java/dtuqac/runtimerapp/MainActivity.java
@@ -1,5 +1,6 @@
 package dtuqac.runtimerapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_timer) {
+            setContentView(R.layout.app_bar_main);
+        } else if (id == R.id.nav_edit_splits) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_save_splits) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_load_splits) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_statistics) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_pebble) {
 
         }
 
